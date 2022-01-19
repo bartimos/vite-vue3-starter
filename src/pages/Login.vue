aef261733685eac43f0ef4b27c9fdb708429d0db
bartimos <bartimos@163.com>
2022-01-19 10:05:21 +0800

    init

---
+++ src/pages/Login.vue
@@ -0,0 +1,23 @@
+<!--  -->
+<template>
+  <div>
+    {{ msg }}<br />
+    长度:{{ mainStore.nameLength }}
+  </div>
+  <button @click="updateName">修改store中的name</button>
+  <div>{{ mainStore.name }}</div>
+</template>
+
+<script setup lang="ts">
+import { useMainStore } from '@/store/main'
+import { ref } from 'vue'
+console.log(import.meta.env.VITE_APP_WEB_URL)
+const msg = ref('this is login page')
+const mainStore = useMainStore()
+const updateName = () => {
+  mainStore.$patch({
+    name: '名称被修改了,nameLength也随之改变了'
+  })
+}
+</script>
+<style lang="scss" scoped></style>
